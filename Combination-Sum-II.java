818602445ff8eb6fb9d2416e71c98b527bcc7d6a
poodsgang69 <jvbprofessional007@gmail.com>
2026-01-30 22:36:42 +0530

    Combination Sum II Recursion Medium - Made it faster by making final_ans local and breaking the loop then the remaining elements are > remaining

--- Combination-Sum-II.java
+++ Combination-Sum-II.java
@@ -1,23 +1,22 @@
 class Solution {
-    private List<List<Integer>> final_ans = new ArrayList<List<Integer>>();
-
-    private void rec(int[] nums, int index, int remaining, List<Integer> curr_ans) {
+    private void rec(int[] nums, int index, int remaining, List<Integer> curr_ans, List<List<Integer>> final_ans) {
         if(remaining < 0) return;
         if(remaining == 0) {
             final_ans.add(new ArrayList<>(curr_ans));
         }
         for(int i = index; i < nums.length ; ++i ) {
             if(i > index && nums[i] == nums[i-1]) continue; // skip the element to avoid duplicate combinations
-            if(nums[i] > remaining) continue;
+            if(nums[i] > remaining) break;
             curr_ans.add(nums[i]); // choose the element
-            rec(nums, i+1, remaining - nums[i], curr_ans);
+            rec(nums, i+1, remaining - nums[i], curr_ans, final_ans);
             curr_ans.remove(curr_ans.size() - 1);
         }
     }
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> final_ans = new ArrayList<List<Integer>>();
         Arrays.sort(candidates);
-        rec(candidates, 0, target, new ArrayList<>());
+        rec(candidates, 0, target, new ArrayList<>(), final_ans);
         return final_ans;
     }
 }
